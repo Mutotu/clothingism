0240e9ad011668e5f08191095de77bc551e1726c
mutotu <muttalip.aydemir@gmail.com>
2022-11-15 09:27:00 -0800

    added functionality to dropdown and cart

--- src/components/card-dropdown/card-dropdown.component.jsx
+++ src/components/card-dropdown/card-dropdown.component.jsx
@@ -1,11 +1,19 @@
+import { useContext } from "react";
+import { CartContext } from "../../context/cart.context";
 import Button from "../button/button.component";
 import "./cart-dropdown.scss";
+import CartItem from "../cart-item/cart-item";
 
 const CartDropdown = () => {
+  const { cartItems } = useContext(CartContext);
   return (
     <div className='cart-dropdown-container'>
-      <div className='cart-items'></div>
-      <Button>Go to Checkout</Button>
+      <div className='cart-items'>
+        {cartItems.map((item) => (
+          <CartItem key={item.id} cartItem={item} />
+        ))}
+        <Button>Go to Checkout</Button>
+      </div>
     </div>
   );
 };
